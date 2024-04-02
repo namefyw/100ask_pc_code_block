2dd4aac5ea5b9dfbac50d99b6f38a5b3798f446d
feng <chat152879@proton.me>
2024-04-03 00:04:55 +0800

    更改ui配置选项

--- lv_100ask_teach_demos/src/lv_my_demo_test/lv_my_demo_test.c
+++ lv_100ask_teach_demos/src/lv_my_demo_test/lv_my_demo_test.c
@@ -5,54 +5,53 @@
 #define COLOR_GREEN lv_color_hex(0xFF008000)
 #define COLOR_BLUE lv_color_hex(0xFF0000FF)
 #define COLOR_WHITE lv_color_hex(0xFFFFFFFF)
-static lv_style_t style1, style2, style3, style4;
-// lv_style_t style1, style2, style3, style4;
+typedef enum {
+    FUNC1,
+    FUNC2,
+    FUNC3,
+    FUNC4,
+    FUNC_MAX
+}obj_count;
+
+typedef struct my_demo_config_x{
+    lv_obj_t* obj[FUNC_MAX];
+}my_demo_config_t;
+lv_style_t style[FUNC_MAX];
+my_demo_config_t my_demo_config;
+
 void lv_my_demo_1()
 {
+    my_demo_config_t* ctx = &my_demo_config;
     // lv_obj_t* windows = lv_scr_act();
     // lv_obj_t* block1 = lv_obj_create(windows);
-    lv_obj_t* block1 = lv_obj_create(lv_scr_act());
-    lv_obj_t* block2 = lv_obj_create(lv_scr_act());
-    lv_obj_t* block3 = lv_obj_create(lv_scr_act());
-    lv_obj_t* block4 = lv_obj_create(lv_scr_act());
+    for (int i = 0; i < FUNC_MAX; i++) {
+        ctx->obj[i] = lv_obj_create(lv_scr_act());
+        lv_obj_t* label = lv_label_create(ctx->obj[i]);
+        lv_label_set_text_fmt(label, "block%d", i);
+        lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    }
 
-    lv_obj_set_size(block1, LV_PCT(50), LV_PCT(50));
-    lv_obj_align(block1, LV_ALIGN_TOP_LEFT, 0, 0);
-    lv_obj_set_size(block2, LV_PCT(50), LV_PCT(50));
-    lv_obj_align(block2, LV_ALIGN_TOP_RIGHT, 0, 0);
-    lv_obj_set_size(block3, LV_PCT(50), LV_PCT(50));
-    lv_obj_align(block3, LV_ALIGN_BOTTOM_LEFT, 0, 0);
-    lv_obj_set_size(block4, LV_PCT(50), LV_PCT(50));
-    lv_obj_align(block4, LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    lv_obj_set_size(ctx->obj[FUNC1], LV_PCT(50), LV_PCT(50));
+    lv_obj_align(ctx->obj[FUNC1], LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_obj_set_size(ctx->obj[FUNC2], LV_PCT(50), LV_PCT(50));
+    lv_obj_align(ctx->obj[FUNC2], LV_ALIGN_TOP_RIGHT, 0, 0);
+    lv_obj_set_size(ctx->obj[FUNC3], LV_PCT(50), LV_PCT(50));
+    lv_obj_align(ctx->obj[FUNC3], LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_set_size(ctx->obj[FUNC4], LV_PCT(50), LV_PCT(50));
+    lv_obj_align(ctx->obj[FUNC4], LV_ALIGN_BOTTOM_RIGHT, 0, 0);
 
-    lv_obj_t* label1 = lv_label_create(block1);
-    lv_obj_t* label2 = lv_label_create(block2);
-    lv_obj_t* label3 = lv_label_create(block3);
-    lv_obj_t* label4 = lv_label_create(block4);
+    lv_style_init(&style[FUNC1]);
+    lv_style_set_bg_color(&style[FUNC1], COLOR_RED);
+    lv_style_init(&style[FUNC2]);
+    lv_style_set_bg_color(&style[FUNC2], COLOR_GREEN);
+    lv_style_init(&style[FUNC3]);
+    lv_style_set_bg_color(&style[FUNC3], COLOR_BLUE);
+    lv_style_init(&style[FUNC4]);
+    lv_style_set_bg_color(&style[FUNC4], COLOR_WHITE);
 
-    lv_label_set_text(label1, "block1");
-    lv_obj_align(label1, LV_ALIGN_CENTER, 0, 0);
-    lv_label_set_text(label2, "block2");
-    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 0);
-    lv_label_set_text(label3, "block3");
-    lv_obj_align(label3, LV_ALIGN_CENTER, 0, 0);
-    lv_label_set_text(label4, "block4");
-    lv_obj_align(label4, LV_ALIGN_CENTER, 0, 0);
-
-
-    lv_style_init(&style1);
-    lv_style_set_bg_color(&style1, COLOR_RED);
-    lv_style_init(&style2);
-    lv_style_set_bg_color(&style2, COLOR_GREEN);
-    lv_style_init(&style3);
-    lv_style_set_bg_color(&style3, COLOR_BLUE);
-    lv_style_init(&style4);
-    lv_style_set_bg_color(&style4, COLOR_WHITE);
-
-    lv_obj_add_style(block1, &style1, LV_PART_MAIN);
-    lv_obj_add_style(block2, &style2, LV_PART_MAIN);
-    lv_obj_add_style(block3, &style3, LV_PART_MAIN);
-    lv_obj_add_style(block4, &style4, LV_PART_MAIN);
+    for (int i = 0; i < FUNC_MAX; i++) {
+        lv_obj_add_style(ctx->obj[i], &style[i], LV_PART_MAIN);
+    }
 
 }
 #endif
